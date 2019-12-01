c980021cb11d8980ad73d7b49da7171243e631e0
Sai Kit Law <saikitlaw@Sais-MacBook-Pro.local>
2019-12-02 00:14:29 +0800

    Second commit

--- server.js
+++ server.js
@@ -540,4 +540,6 @@ const findPhoto = (db,criteria,callback) => {
 	
 app.use(express.static(__dirname +  '/public'));
 
-app.listen(process.env.PORT || 8099);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
